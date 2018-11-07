93a9727ef2689bcfc8360d17d845b918d4c22d8e
Denny <denny.az45@gmail.com>
2018-11-07 16:20:14 +0700

    Initial commit

---
+++ src/newpackage/KelasKoneksi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Denny
+ */
+public class KelasKoneksi {
+    private static Connection koneksi ;
+    
+    public static Connection getConnection() throws SQLException{
+        if(koneksi==null){
+            new Driver();
+            koneksi = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_latgambar?zeroDateTimeBehavior=convertToNull", "root", "");
+        }
+        return koneksi;   
+    }
+}
